35b9ea48929048cade06dda0991f92ce388ced3f
samit <samitpal@yahii.com>
2018-05-13 13:33:29 -0400

    Add rest endpoint

---
+++ src/main/java/org/openshift/InsultResources.java
@@ -0,0 +1,20 @@
+package org.openshift;
+
+import javax.faces.bean.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.xml.ws.RequestWrapper;
+import java.util.HashMap;
+
+@RequestScoped
+@Path("/insult")
+public class InsultResources {
+	@GET
+	@Produces("application/json")
+	public HashMap<String, String> getInsult(){
+		HashMap<String, String> theInsult = new HashMap<>();
+		theInsult.put("insult", new InsultGenerator().generateInsult());
+		return theInsult;
+	}
+}

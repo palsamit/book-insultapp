3f6d53b96681b9fbb139b7c717bb03cd871149ff
samit <samitpal@yahii.com>
2018-05-13 09:11:47 -0400

    add postgresql

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,5 +1,6 @@
 package org.openshift;
 
+import java.sql.*;
 import java.util.Random;
 
 public class InsultGenerator {
@@ -7,13 +8,38 @@ public class InsultGenerator {
 		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
 		String vowels = "AEIOU";
 		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
+		String theInsult = "";
+
+		try {
+			String databaseUrl = "jdbc:postgresql://"
+					+ System.getenv("POSTGRESQL_SERVICE_HOST")
+					+ "/" + System.getenv("POSTGRESQL_DATABASE");
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
+
+			Connection connection = DriverManager.getConnection(databaseUrl, username, password);
+			if( connection != null ) {
+				String sqlQuery = "select a.string as first, b.string as second, c.string as third from short_adjective a"
+						+ " long_adjective b, noun c order by random() limit 1";
+				Statement statement = connection.createStatement();
+				ResultSet rs = statement.executeQuery(sqlQuery);
+				while( rs.next()) {
+					if( vowels.indexOf(rs.getString("first").charAt(0)) == -1){
+						article = "a";
+					}
+					theInsult = String.format("Thou art %s %s %s %s!"
+							, article, rs.getString("first"), rs.getString("second")
+							, rs.getString("third"));
+				}
+				rs.close();
+				statement.close();
+				connection.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+		return theInsult;
 	}
 
 }
